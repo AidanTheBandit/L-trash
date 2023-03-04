da0dab3cacdd962c8bf098e997f4a9c572dbacf9
AidanTheBandit <aidanthebandit@icloud.com>
2023-03-04 11:47:08 -0500

    Inintial commit

---
+++ theme/src/config/footer.js
@@ -0,0 +1,40 @@
+export const footerSocials = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/BanditCo_',
+    icon: "mdi:twitter",
+  },
+]
+
+export const footerLists = [
+	{
+		title: 'Landing Pages',
+		items: [
+			{
+				title: 'Landing Page 1',
+				slug: '/landing-pages/landing-1',
+			},
+			{
+				title: 'Landing Page 2',
+				slug: '/landing-pages/landing-2',
+			},
+      // {
+			// 	title: 'Landing Page 3',
+			// 	slug: '/landing-pages/landing-3',
+			// },
+		],
+	},
+	{
+		title: 'Company',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+];
---
+++ theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `L+Trash is a more sustainable way to throw away.`,
+	description: `L+Trash is a more sustainable way to throw away.`,
+	url: `https://ltrash.bndt.cloud`, // No trailing slash!
+	name: `L+Trash`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
